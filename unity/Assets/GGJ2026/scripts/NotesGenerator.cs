668b79922875f071411f0825056b702a30d8065c
Sean <sdunlap0309@gmail.com>
2026-01-31 12:31:18 -0500

    Update NotesGenerator generate notes based on json script.

--- unity/Assets/GGJ2026/scripts/NotesGenerator.cs
+++ unity/Assets/GGJ2026/scripts/NotesGenerator.cs
@@ -1,41 +1,34 @@
 #nullable enable
 
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace GGJ2026
 {
-    //TODO: Behavior commented out for now. Is this even needed?
     public class NotesGenerator : MonoBehaviour
     {
-        public UnityEvent<int> SpawnNote { get; private set; } = new();
-        
-        private MusicScript? _noteScript;
-        private float _scriptStart;
+        public UnityEvent<int> SpawnNote = new();
         
         private void Start()
         {
-            // _scriptStart = Time.time;
-            //
-            // //initialize with test values
-            // var notes = new List<NoteData>();
-            //
-            // _noteScript = new MusicScript { Notes = notes };
+            var asset = "Assets/GGJ2026/test-song-data.json";
+            var script = FileParser.Parse(asset);
+
+            foreach (var note in script.Notes)
+            {
+                var absoluteBeat = note.Bar * 4 + note.Beat;
+                var time = absoluteBeat / (script.BeatsPerMinute / 60);
+
+                StartCoroutine(spawnNote(note.NoteId, (float)time));
+            }
         }
         
-        // Update is called once per frame
-        private void Update()
+        private IEnumerator spawnNote(int id, float delay)
         {
-            // float time = Time.time - _scriptStart;
-            // if (_noteScript is null) { return; }
-            // if (_noteScript.Notes.Any() == false) { return; }
-            // while (_noteScript.Notes.First().Bar <= time)
-            // {
-            //     SpawnNote.Invoke(_noteScript.Notes.First().NoteId);
-            //     _noteScript.Notes.RemoveAt(0);
-            // }
+            yield return new WaitForSeconds(delay);
+
+            SpawnNote.Invoke(id);
         }
     }
 }
