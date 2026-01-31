0bfdfe6428fab1d8d0353e9863c56a987b7381c4
Dan Vicarel <dan.vicarel@gmail.com>
2026-01-30 21:49:36 -0500

    Add basic scene setup for notes

---
+++ unity/Assets/GGJ2026/scripts/NoteConsumer.cs
@@ -0,0 +1,35 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityUtil.Updating;
+
+#nullable enable
+
+namespace GGJ2026
+{
+    public class NoteIndicatorMover : Updatable
+    {
+        private Transform[]? _noteTransforms;
+
+        [RequiredIn(PrefabKind.PrefabInstanceAndNonPrefabInstance)]
+        public Transform NotesParent;
+
+        public Vector3 MoveDirection = Vector3.left;
+
+        public float MoveSpeed = 5f;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _noteTransforms = NotesParent.GetComponentsInChildren<Transform>();
+
+            AddUpdate(updateNotes);
+        }
+
+        private void updateNotes(float deltaTime)
+        {
+            foreach (Transform noteTransform in _noteTransforms!)
+                noteTransform.position += MoveSpeed * deltaTime * MoveDirection;
+        }
+    }
+}
