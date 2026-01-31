1b0dfd2b49987d9f87e8096d8544610376c14fe4
Dan Vicarel <dan.vicarel@gmail.com>
2026-01-31 18:59:46 -0500

    Finalize character animations

--- unity/Assets/GGJ2026/scripts/CharacterSpawner.cs
+++ unity/Assets/GGJ2026/scripts/CharacterSpawner.cs
@@ -1,49 +1,81 @@
 #nullable enable
 
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace GGJ2026
 {
+    public enum CharacterType
+    {
+        None,
+        OldLady,
+        DoorDasher,
+        Businessman,
+    }
     public class CharacterSpawner : MonoBehaviour
     {
-        [SerializeField, ListDrawerSettings(ShowFoldout = false, IsReadOnly = true)]
-        private readonly Character?[] _characters = Enumerable.Repeat((Character?)null, Constants.CharacterCount).ToArray();
+        private readonly Dictionary<CharacterType, Character> _shownCharacters = new();
 
         [SerializeField, RequiredIn(PrefabKind.PrefabInstanceAndNonPrefabInstance)]
-        private Transform? _charactersParent;
+        private Character? _oldLady;
+        [SerializeField, RequiredIn(PrefabKind.PrefabInstanceAndNonPrefabInstance)]
+        private Character? _doorDasher;
+        [SerializeField, RequiredIn(PrefabKind.PrefabInstanceAndNonPrefabInstance)]
+        private Character? _businessman;
 
-        [SerializeField] private Vector2 _characterStartPosition = new(-1000f, -200f);
-
+        [SerializeField] private string _characterEnterAnimStateName = "enter";
         [SerializeField] private string _characterExitAnimStateName = "exit";
-        [SerializeField] private string _characterMistakeAnimStateName = "mistake";
 
-        public void ShowCharacter(int index)
+        [Header("Test Spawning")]
+
+        [SerializeField] private string _spawnOldLadyInputActionName = "spawn-old-lady";
+        [SerializeField] private string _spawnDoorDasherInputActionName = "spawn-door-dasher";
+        [SerializeField] private string _spawnBusinessManInputActionName = "spawn-businessman";
+
+        private void Awake()
         {
-            Character character = _characters[index]!;
-            Debug.Log($"Showing character named '{character.name}'...");
-
-            character.transform.position = _characterStartPosition;
-            character.gameObject.SetActive(true);
+            InputSystem.actions[_spawnOldLadyInputActionName].performed += ctx => ShowCharacter(CharacterType.OldLady);
+            InputSystem.actions[_spawnDoorDasherInputActionName].performed += ctx => ShowCharacter(CharacterType.DoorDasher);
+            InputSystem.actions[_spawnBusinessManInputActionName].performed += ctx => ShowCharacter(CharacterType.Businessman);
         }
 
-        public void ShowCharacterWrongEffects()
+        [Button]
+        public void ShowCharacter(CharacterType type)
         {
-            foreach (Character? character in _characters) {
-                if (character == null || !character.gameObject.activeSelf)
-                    continue;
+            Debug.Log($"Showing character '{type}'...");
 
-                character!.AnimatorStateRestarter.RestartCurrentState();
-                character.Animator.Play(_characterMistakeAnimStateName);
+            Character character = type switch {
+                CharacterType.OldLady => _oldLady!,
+                CharacterType.DoorDasher => _doorDasher!,
+                CharacterType.Businessman => _businessman!,
+                _ => throw new NotImplementedException(),
+            };
+
+            character.gameObject.SetActive(true);
+            character.Animator.Play(_characterEnterAnimStateName);
+            _shownCharacters[type] = character;
+        }
+
+        [Button]
+        public void ShowCharacterMistakeEffects()
+        {
+            foreach (Character character in _shownCharacters.Values) {
+                character.AnimatorStateRestarter.RestartCurrentState();
+                character.Animator.Play(character.MistakeAnimStateName);
             }
         }
 
-        public void HideCharacter(int index)
+        [Button]
+        public void HideCharacter(CharacterType type)
         {
-            Character character = _characters[index]!;
+            Debug.Log($"Hiding character '{type}'...");
+
+            Character character = _shownCharacters[type];
             character.Animator.Play(_characterExitAnimStateName);
+            _shownCharacters.Remove(type);
         }
     }
 }
