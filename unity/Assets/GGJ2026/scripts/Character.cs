45ef9bf4eef8716fdbed2395cc0a3dff9fae7648
Dan Vicarel <dan.vicarel@gmail.com>
2026-01-31 18:59:46 -0500

    Finalize character animations

--- unity/Assets/GGJ2026/scripts/Character.cs
+++ unity/Assets/GGJ2026/scripts/Character.cs
@@ -5,6 +5,7 @@ namespace GGJ2026
 {
     public class Character : MonoBehaviour
     {
+        [field: SerializeField] public string MistakeAnimStateName { get; private set; } = "mistake";
         [field: SerializeField] public Animator Animator { get; private set; }
         [field: SerializeField] public AnimatorStateRestarter AnimatorStateRestarter { get; private set; }
     }
