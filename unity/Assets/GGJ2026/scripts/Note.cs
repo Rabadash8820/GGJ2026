7f094bfc3ced0fca9569422da69cb05dbb173354
Dan Vicarel <dan.vicarel@gmail.com>
2026-01-31 00:34:02 -0500

    Remove the `NoteType` enum. We'll just use int indices for simplicity

--- unity/Assets/GGJ2026/scripts/Note.cs
+++ unity/Assets/GGJ2026/scripts/Note.cs
@@ -5,14 +5,6 @@ namespace GGJ2026
 {
     public class Note : MonoBehaviour
     {
-        public UnityEvent<NoteType> SpawnNote { get; set; }
-        
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start() { }
-
-        // Update is called once per frame
-        void Update()
-        {
-        }
+        public UnityEvent<int> SpawnNote { get; set; }
     }
 }
