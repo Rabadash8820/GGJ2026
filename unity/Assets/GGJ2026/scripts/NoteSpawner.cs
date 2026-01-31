f582ca6ecd6520eb16d592bb0910001d1578eedf
Sean <sdunlap0309@gmail.com>
2026-01-31 14:57:21 -0500

    Simple indicator for held notes.

--- unity/Assets/GGJ2026/scripts/NoteSpawner.cs
+++ unity/Assets/GGJ2026/scripts/NoteSpawner.cs
@@ -14,6 +14,9 @@ namespace GGJ2026
     {
         private readonly Queue<NoteIndicator>[] _notePools =
             Enumerable.Range(0, NoteConstants.NoteCount).Select(x => new Queue<NoteIndicator>()).ToArray();
+        private readonly Queue<NoteIndicator>[] _noteHeldPools = 
+            Enumerable.Range(0, NoteConstants.NoteCount).Select(x => new Queue<NoteIndicator>()).ToArray();
+
         private readonly int[] _noteCounts = Enumerable.Repeat(0, NoteConstants.NoteCount).ToArray();
 
         private readonly List<NoteIndicator> _shownNotes = new();
@@ -28,8 +31,14 @@ namespace GGJ2026
         [SerializeField]
         private string _noteNameFormat = "note-{0}-{1}";
 
+        [SerializeField] 
+        private string _noteHeldNameFormat = "note-{0}-{1}-held";
+
         [SerializeField, ListDrawerSettings(ShowFoldout = false, IsReadOnly = true)]
         private GameObject?[] _notePrefabs = new GameObject[NoteConstants.NoteCount];
+
+        [SerializeField, ListDrawerSettings(ShowFoldout = false, IsReadOnly = true)]
+        private GameObject?[] _noteHeldPrefabs = new GameObject[NoteConstants.NoteCount];
 
         [SerializeField, ListDrawerSettings(ShowFoldout = false, IsReadOnly = true), Min(0f)]
         private Vector2[] _noteStartPositions = Enumerable
@@ -44,7 +53,10 @@ namespace GGJ2026
             for (int n = 0; n < NoteConstants.NoteCount; n++) {
                 Debug.Log($"Spawning {_initialNoteCount} initial notes of type {n}...");
                 for (int i = 0; i < _initialNoteCount; i++)
+                {
                     _notePools[n].Enqueue(instantiateNote(n));
+                    _noteHeldPools[n].Enqueue(instantiateNote(n, true));                    
+                }
             }
         }
 
@@ -55,10 +67,19 @@ namespace GGJ2026
             NoteIndicator noteIndicator = _notePools[noteData.NoteId].TryDequeue(out noteIndicator) 
                 ? noteIndicator : instantiateNote(noteData.NoteId);
             noteIndicator.gameObject.SetActive(true);
-
             _shownNotes.Add(noteIndicator);
-
-            NoteSpawned.Invoke(noteIndicator!);
+            NoteSpawned.Invoke(noteIndicator);
+            
+            if (noteData.Duration > 1)
+            {
+                NoteIndicator heldNoteIndicator = _noteHeldPools[noteData.NoteId].TryDequeue(out heldNoteIndicator)
+                    ? heldNoteIndicator : instantiateNote(noteData.NoteId, true);
+                heldNoteIndicator.gameObject.SetActive(true);
+                heldNoteIndicator.transform.localScale = new Vector3(noteData.Duration, 1, 1);
+                heldNoteIndicator.transform.position += new Vector3(-noteData.Duration * 64, 0, 0);
+                _shownNotes.Add(noteIndicator);
+                NoteSpawned.Invoke(heldNoteIndicator);
+            }
         }
 
         public void ReturnNoteToPool(NoteIndicator noteIndicator)
@@ -70,15 +91,21 @@ namespace GGJ2026
             _notePools[noteIndicator.NoteIndex].Enqueue(noteIndicator);
         }
 
-        private NoteIndicator instantiateNote(int noteIndex)
+        private NoteIndicator instantiateNote(int noteIndex, bool held = false)
         {
             Debug.Log($"Spawning note of type {noteIndex}...");
 
-            GameObject noteObject = Instantiate(_notePrefabs[noteIndex]!, _noteStartPositions[noteIndex], Quaternion.identity, _notesParent!);
+            var nameFormat = held ? _noteHeldNameFormat: _noteNameFormat;
+            var prefabs = held ? _noteHeldPrefabs : _notePrefabs;
+
+            var startPositions = _noteStartPositions[noteIndex];
+            var position3 = new Vector3(startPositions.x, startPositions.y, held ? 0 : 1);
+            
+            GameObject noteObject = Instantiate(prefabs[noteIndex]!, position3, Quaternion.identity, _notesParent!);
             NoteIndicator noteIndicator = noteObject.GetComponentInChildren<NoteIndicator>();
             Assert.IsTrue(noteIndicator != null);
 
-            noteObject.name = string.Format(_noteNameFormat, noteIndex, _noteCounts[noteIndex]);
+            noteObject.name = string.Format(nameFormat, noteIndex, _noteCounts[noteIndex]);
             noteObject.SetActive(false);
 
             ++_noteCounts[noteIndex];
