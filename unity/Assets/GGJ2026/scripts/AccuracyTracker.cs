6fc7a9c4f3405feedbf4121b2509f56446c8ab3d
Dan Vicarel <dan.vicarel@gmail.com>
2026-02-01 07:16:11 -0500

    Implement the "accuracy faces" in the HUD

---
+++ unity/Assets/GGJ2026/scripts/AccuracyTracker.cs
@@ -0,0 +1,42 @@
+#nullable enable
+
+using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GGJ2026
+{
+    public class AccuracyTracker : MonoBehaviour
+    {
+        [SerializeField, RequiredIn(PrefabKind.PrefabInstanceAndNonPrefabInstance)]
+        private NoteHitter? _noteHitter;
+
+        [SerializeField, RequiredIn(PrefabKind.PrefabInstanceAndNonPrefabInstance)]
+        private NotesGenerator? _notesGenerator;
+
+        [SerializeField, RequiredIn(PrefabKind.PrefabInstanceAndNonPrefabInstance)]
+        private SpriteRenderer? _faceSpriteRenderer;
+
+        [SerializeField] private Sprite[] _faceSprites = Array.Empty<Sprite>();
+
+        [SerializeField] private float _minAccuracy = 0.15f;
+
+        [SerializeField] private int _minNotesBeforeTooLow = 10;
+
+        [SerializeField] private UnityEvent _accuracyTooLow = new();
+
+        public void UpdateAccuracy()
+        {
+            float accuracy = (float)_noteHitter!.NotesHitCount / _notesGenerator!.GeneratedNoteCount;
+
+            int faceSpriteIndex = (int)(accuracy * _faceSprites.Length);
+            _faceSpriteRenderer!.sprite = _faceSprites[faceSpriteIndex];
+
+            if (accuracy < _minAccuracy && _notesGenerator!.GeneratedNoteCount >= _minNotesBeforeTooLow) {
+                Debug.Log($"Accuracy ({accuracy:P}) fell below the min ({_minAccuracy:P})");
+                _accuracyTooLow.Invoke();
+            }
+        }
+    }
+}
